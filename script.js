c9c9a18da958c8362da1f44f224a314aabc1c9fe
tusharmore26 <ajaymore0634@gmail.com>
2023-11-24 13:28:36 +0530

    final commit

---
+++ script.js
@@ -0,0 +1,143 @@
+
+let svg_Obj = {
+    0: '<svg stroke="currentColor" fill="currentColor" stroke-width="0" viewBox="0 0 384 512" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg"><path d="M172.268 501.67C26.97 291.031 0 269.413 0 192 0 85.961 85.961 0 192 0s192 85.961 192 192c0 77.413-26.97 99.031-172.268 309.67-9.535 13.774-29.93 13.773-39.464 0zM192 272c44.183 0 80-35.817 80-80s-35.817-80-80-80-80 35.817-80 80 35.817 80 80 80z"></path></svg>',
+    1 : '<svg stroke="currentColor" fill="currentColor" stroke-width="0" viewBox="0 0 512 512" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg"><path d="M459.37 151.716c.325 4.548.325 9.097.325 13.645 0 138.72-105.583 298.558-298.558 298.558-59.452 0-114.68-17.219-161.137-47.106 8.447.974 16.568 1.299 25.34 1.299 49.055 0 94.213-16.568 130.274-44.832-46.132-.975-84.792-31.188-98.112-72.772 6.498.974 12.995 1.624 19.818 1.624 9.421 0 18.843-1.3 27.614-3.573-48.081-9.747-84.143-51.98-84.143-102.985v-1.299c13.969 7.797 30.214 12.67 47.431 13.319-28.264-18.843-46.781-51.005-46.781-87.391 0-19.492 5.197-37.36 14.294-52.954 51.655 63.675 129.3 105.258 216.365 109.807-1.624-7.797-2.599-15.918-2.599-24.04 0-57.828 46.782-104.934 104.934-104.934 30.213 0 57.502 12.67 76.67 33.137 23.715-4.548 46.456-13.32 66.599-25.34-7.798 24.366-24.366 44.833-46.132 57.827 21.117-2.273 41.584-8.122 60.426-16.243-14.292 20.791-32.161 39.308-52.628 54.253z"></path></svg>',
+    2 : '<svg stroke="currentColor" fill="currentColor" stroke-width="0" viewBox="0 0 512 512" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg"><path d="M320 336c0 8.84-7.16 16-16 16h-96c-8.84 0-16-7.16-16-16v-48H0v144c0 25.6 22.4 48 48 48h416c25.6 0 48-22.4 48-48V288H320v48zm144-208h-80V80c0-25.6-22.4-48-48-48H176c-25.6 0-48 22.4-48 48v48H48c-25.6 0-48 22.4-48 48v80h512v-80c0-25.6-22.4-48-48-48zm-144 0H192V96h128v32z"></path></svg>',
+    3: '<svg stroke="currentColor" fill="currentColor" stroke-width="0" viewBox="0 0 512 512" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg"><path d="M502.3 190.8c3.9-3.1 9.7-.2 9.7 4.7V400c0 26.5-21.5 48-48 48H48c-26.5 0-48-21.5-48-48V195.6c0-5 5.7-7.8 9.7-4.7 22.4 17.4 52.1 39.5 154.1 113.6 21.1 15.4 56.7 47.8 92.2 47.6 35.7.3 72-32.8 92.3-47.6 102-74.1 131.6-96.3 154-113.7zM256 320c23.2.4 56.6-29.2 73.4-41.4 132.7-96.3 142.8-104.7 173.4-128.7 5.8-4.5 9.2-11.5 9.2-18.9v-19c0-26.5-21.5-48-48-48H48C21.5 64 0 85.5 0 112v19c0 7.4 3.4 14.3 9.2 18.9 30.6 23.9 40.7 32.4 173.4 128.7 16.8 12.2 50.2 41.8 73.4 41.4z"></path></svg>',
+    4: '<svg stroke="currentColor" fill="currentColor" stroke-width="0" viewBox="0 0 512 512" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg"><path d="M326.612 185.391c59.747 59.809 58.927 155.698.36 214.59-.11.12-.24.25-.36.37l-67.2 67.2c-59.27 59.27-155.699 59.262-214.96 0-59.27-59.26-59.27-155.7 0-214.96l37.106-37.106c9.84-9.84 26.786-3.3 27.294 10.606.648 17.722 3.826 35.527 9.69 52.721 1.986 5.822.567 12.262-3.783 16.612l-13.087 13.087c-28.026 28.026-28.905 73.66-1.155 101.96 28.024 28.579 74.086 28.749 102.325.51l67.2-67.19c28.191-28.191 28.073-73.757 0-101.83-3.701-3.694-7.429-6.564-10.341-8.569a16.037 16.037 0 0 1-6.947-12.606c-.396-10.567 3.348-21.456 11.698-29.806l21.054-21.055c5.521-5.521 14.182-6.199 20.584-1.731a152.482 152.482 0 0 1 20.522 17.197zM467.547 44.449c-59.261-59.262-155.69-59.27-214.96 0l-67.2 67.2c-.12.12-.25.25-.36.37-58.566 58.892-59.387 154.781.36 214.59a152.454 152.454 0 0 0 20.521 17.196c6.402 4.468 15.064 3.789 20.584-1.731l21.054-21.055c8.35-8.35 12.094-19.239 11.698-29.806a16.037 16.037 0 0 0-6.947-12.606c-2.912-2.005-6.64-4.875-10.341-8.569-28.073-28.073-28.191-73.639 0-101.83l67.2-67.19c28.239-28.239 74.3-28.069 102.325.51 27.75 28.3 26.872 73.934-1.155 101.96l-13.087 13.087c-4.35 4.35-5.769 10.79-3.783 16.612 5.864 17.194 9.042 34.999 9.69 52.721.509 13.906 17.454 20.446 27.294 10.606l37.106-37.106c59.271-59.259 59.271-155.699.001-214.959z"></path></svg>',
+}
+
+let stats_obj = {
+   
+    0:'Followers',
+    1:'Following',
+    2:'Repos'
+}
+
+async  function getUser(user)
+{
+    let result ;
+    let url ="https://api.github.com/users/"+user;
+    await fetch(url)
+    .then((res)=>(res.json()))
+    .then((data)=>{
+      result=data;
+    })
+    .catch((e)=>{console.log(e);})   
+    return result;   
+}
+
+function addElement(user)
+{
+    
+     span_obj={
+        0:user['location']?user['location']:'Unavailable',
+        1: user['twitter_username']?user['twitter_username']:'Unavailable',
+        2: user['company']?user['company']:'Unavailable',
+        3: user['email']?user['email']:'Unavailable',
+        4: user['blog']?user['blog']:'Unavailable',
+     }
+      stat={
+        0:user['followers']?user['followers']:'Unavailable',
+        1:user['following']?user['following']:'Unavailable',
+        2:user['public_repos']?user['public_repos']:'Unavailable',
+      }
+
+    const div = document.createElement('div');
+    div.className='user-container';
+
+    const user_left = document.createElement('div');
+    user_left.className = 'user-left';
+
+    img = document.createElement('img');
+    img.setAttribute('src',user['avatar_url'])
+    img.setAttribute('alt',user['name']+'avatar')
+    user_left.appendChild(img)
+
+
+    const div_info = document.createElement('div')
+    div_info.className="user-information"
+
+    for(let i=0;i<5;i++)
+    {
+        const div = document.createElement('div');
+        div.className = "user-field"
+        div.innerHTML= svg_Obj[i]+'<span>'+span_obj[i]+'</span>';
+     
+
+        div_info.appendChild(div);
+    }
+    user_left.appendChild(div_info)
+    div.appendChild(user_left)
+
+    const user_right = document.createElement('div');
+    user_right.className = 'user-right';
+
+    h1= document.createElement('h1')
+    a=document.createElement('a')
+    a.setAttribute('href',user['html_url'])
+    a.innerHTML=user['name']
+    h1.appendChild(a) 
+    user_right.appendChild(h1)
+
+    p=document.createElement('p')
+    p.innerHTML='Joined On '+ (user['created_at'].split('T')[0])    
+    user_right.appendChild(p);
+
+    h3 = document.createElement('h3');
+    h3.innerHTML=user['bio']
+    user_right.appendChild(h3);
+
+    user_stats = document.createElement('div')
+    user_stats.className = "user-stats"
+    for(let i=0;i<3;i++)
+    {
+        const div =  document.createElement('div')
+        div.className='stats'
+
+        h4= document.createElement('h4');
+        h4.innerHTML=stats_obj[i];
+        div.appendChild(h4)
+
+        h5= document.createElement('h5');
+        h5.innerHTML=stat[i];
+        div.appendChild(h5)
+
+        user_stats.appendChild(div)
+    }
+    
+    user_right.appendChild(user_stats)
+
+
+   
+
+
+    div.appendChild(user_right)
+    
+    document.querySelector('.container').appendChild(div)
+}
+
+let form = document.querySelector('form')
+let user;
+form.addEventListener('submit', async (e)=>{
+   
+    e.preventDefault();
+   
+   
+    user = form.querySelector('input').value      
+    const data = await  getUser(user); 
+    
+    addElement(data);     
+
+})
+input = form.querySelector('button')
+
+input.addEventListener('click',()=>{
+    const newChild=document.querySelector('.user-container')
+    if(newChild)
+    {
+        document.querySelector('.container').removeChild(newChild)
+    }
+    // alert('changed')
+    
+})
